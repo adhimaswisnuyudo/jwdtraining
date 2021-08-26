59de66f545cd43897da878be5ca25fcee16b0455
adhimaswisnuyudo <adhimas93@ymail.com>
2021-08-26 08:12:18 +0700

    hari pertama

---
+++ application/controllers/Account.php
@@ -0,0 +1,12 @@
+<?php
+
+class Account extends CI_Controller{
+    
+    function login(){
+        echo "Halaman Login";
+    }
+
+    function tampildata($data=NULL){
+        echo $data;
+    }
+}
